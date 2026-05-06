db202fcc2ff31ce86f7ce73383d053c44cd2bb79
Yash-Raj-5424 <myash3499@gmail.com>
2026-05-06 15:12:46 +0530

    initial setup

---
+++ src/test/java/com/pay/outbox/OutboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pay.outbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
